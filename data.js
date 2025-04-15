9869342f5e900b60d616c3c250e83e33c5c0b49e
hxasset <hxasset0307@gmail.com>
2025-04-15 14:14:22 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,99 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-l",
+      "name": "L",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 452.5,
+      "initialViewParameters": {
+        "yaw": -0.08841351338698189,
+        "pitch": -0.030596255709202325,
+        "fov": 1.8643219839731817
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4397672550030602,
+          "pitch": 0.005022818199972079,
+          "rotation": 5.497787143782138,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.078531422208945,
+          "pitch": 0.1819692534849704,
+          "title": "格局圖",
+          "text": "<img src='floorplan1.jpg' style='width:100%; height:auto;' />"
+        },
+        {
+          "yaw": 1.895632903246863,
+          "pitch": 0.3853533780771752,
+          "title": "匯幸資產版權所有",
+          "text": "202504拍攝"
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3235734713485625,
+          "pitch": 0.2457421951398029,
+          "rotation": 0.7853981633974483,
+          "target": "0-l"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.103009701023325,
+          "pitch": -0.03809545787125046,
+          "title": "格局圖",
+          "text": "<img src='floorplan2.jpg' style='width:100%; height:auto;' />"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
